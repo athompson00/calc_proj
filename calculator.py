a170933319fe72926cd9d0ec0a5031c8ec8d0d98
athompson00 <athompson00@stuy.edu>
2020-07-09 09:40:14 -0400

    beep boopnig

--- calculator.py
+++ calculator.py
@@ -1,5 +1,14 @@
 import definitions
 # The line above will let you separate your concerns by defining functions your calculator might use in a separate file.
+print("The program is running")
+print("Welcome to the Python Calculator\n\nEnter an operator: ")
+operator = input()
 
-print("Welcome to the Python Calculator\n\nWhat would you like to do?")
-choice = input()
+args = input("Enter args separated by commas (ex: 3, 4, 5): ")
+newArgs = args.split(',')
+#print(newArgs)
+x = 0
+for arg in newArgs:
+    newArgs[x] = arg.replace(" ", "")
+    x+=1
+#print(newArgs)
