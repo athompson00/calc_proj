b291bd21c6686ce4304817a396a0c35ac57d618d
athompson00 <athompson00@stuy.edu>
2020-07-09 21:19:23 -0400

    completed project with multivariable parsing and full functionality

--- calculator.py
+++ calculator.py
@@ -1,61 +1,39 @@
-import definitions
+#import definitions
 from math import *
-def derive(function, value):
-    h = 0.00000000001
-    top = function(value + h) - function(value)
-    bottom = h
-    slope = top / bottom    # Returns the slope to the third decimal
-    return int(float("%.3f" % slope))
-# The line above will let you separate your concerns by defining functions your calculator might use in a separate file.
-print("The program is running")
-print("Welcome to the Python Derivative Calculator\n\nTo Begin, Enter an expression (format: n * x ^ p + n * x ^ p): ")
-expression = input()
-args = expression.split()
-# print(expression)
-# formattedExpression = 0
-# print(type(int('string')))
-#for char in expression:
-def f(n):
-    newExp = 0
+from sympy import *
+from py_expression_eval import Parser
+
+
+#print("The program is running")
+print("Welcome to the Python Derivative Calculator\n\n")
+run = True
+while run:
+    print("To Begin, Enter an expression (include all operators): ")
+    expression = input()
+    parser = Parser()
+    expr = parser.parse(expression)
+    my_symbols = {}
+    optionslist = ""
     counter = 0
-    while counter < len(args):
-        if args[counter].isdigit():
-            if (counter != len(args) - 1) and "^" in args[counter+1]:
-                newExp += int(args[counter])**args[counter+2]
-                counter += 2
-            else:
-                newExp += int(args[counter])
-        if args[counter] == 'x':
-            if (counter != len(args) - 1) and "^" in args[counter+1]:
-                newExp += n**int(args[counter+2])
-            else:
-                newExp += int(n)
-        if args[counter] == '+':
-            if args[counter+1] == 'x':
-                newExp += n
-            else:
-                newExp += int(args[counter+1])
-            counter+=1
-        if args[counter] == '-':
-            if args[counter+1] == 'x':
-                newExp -= n
-            else:
-                newExp -= int(args[counter+1])
-            counter += 1
-        if args[counter] == '*':
-            if args[counter+1] == 'x':
-                newExp *= n
-            else:
-                newExp *= int(args[counter+1])
-            ++counter
-        if args[counter] == '/':
-            if args[counter+1] == 'x':
-                newExp = newExp / n
-            else:
-                newExp = newExp / int(args[counter+1])
-            counter+=1
-        counter += 1
-    print(newExp)
-    return newExp
-value = int(input("Enter an x value of the derivative: "))
-print(derive(f, value))
+    for var in expr.variables():
+        my_symbols[var] = Symbol(var, real=True)
+        if (counter == len(expr.variables()) - 1):
+            optionslist += var
+        else:
+            optionslist += var + ", "
+        counter+=1
+    real_func = sympify(expr.toString(), my_symbols)
+    dvar = input("What variable would you like to differentiate by?\n" + "Options: " + optionslist + '\n')
+    if (dvar in my_symbols):
+        diffated = diff(real_func, my_symbols[dvar])
+    else:
+        dvar = input("That isn't an option. Enter a new one.\n" + "Options: " + optionslist + '\n')
+    print("The derivative is: " + str(diffated))
+    print("What value would you like to plug in for the differentiated variable?")
+
+    value = int(input())
+    print("Your answer is: " + str(diffated.subs(my_symbols[dvar], value)))
+    print("Would you like to try another function? (Y/N): ")
+    rbool = input()
+    if rbool.upper() == 'N':
+        run = False
